2c86e3c34e1c853e0988c33b9729e9f3c67b255f
Victor Hugo Alves <80076522+VictorHugoMA@users.noreply.github.com>
2021-08-30 20:38:47 -0300

    add Classe Pessoa

---
+++ src/Pessoa.java
@@ -0,0 +1,42 @@
+
+public class Pessoa {
+	private String nome;
+	private String cpf, rg;
+	private byte estadoCivil;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getRg() {
+		return rg;
+	}
+	
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public byte getEstadoCivil() {
+		return estadoCivil;
+	}
+	
+	public void setEstadoCivil(byte estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+	
+	
+	
+	
+}
