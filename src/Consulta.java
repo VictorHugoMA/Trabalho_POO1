dfdfce1f7d55672c25512e51320c1fc99fecb3a2
VictorHugoMA <victor.alves1@ufu.br>
2021-09-02 11:32:22 -0300

    add verificacao no set

--- src/Consulta.java
+++ src/Consulta.java
@@ -36,7 +36,8 @@ public class Consulta {
 		return formato2.format(dataHoraCons);
 	}
 	
-	public void setStrDataAdmi(String dataHoraCons) throws ParseException {
+	
+	public void setStrDataHoraCons(String dataHoraCons) throws ParseException {
 		this.dataHoraCons = formato2.parse(dataHoraCons);
 	}
 	
@@ -45,51 +46,80 @@ public class Consulta {
 	public ArrayList<String> getMedicamentos() {
 		return medicamentos;
 	}
+	
 	public void setMedicamentos(ArrayList<String> medicamentos) {
 		this.medicamentos = medicamentos;
 	}
+	
+	
 	public String getObsSintomas() {
 		return obsSintomas;
 	}
+	
 	public void setObsSintomas(String obsSintomas) {
 		this.obsSintomas = obsSintomas;
 	}
+	
+	
 	public String getReceita() {
 		return receita;
 	}
+	
 	public void setReceita(String receita) {
 		this.receita = receita;
 	}
+	
+	
 	public float getValor() {
 		return valor;
 	}
-	public void setValor(float valor) {
-		this.valor = valor;
+	
+	public boolean setValor(float valor) {
+		if(valor>0) {
+			this.valor = valor;
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
+	
+	
 	public DemaisFunc getFuncMarc() {
 		return funcMarc;
 	}
+	
 	public void setFuncMarc(DemaisFunc funcMarc) {
 		this.funcMarc = funcMarc;
 	}
+	
+	
 	public Medico getMedico() {
 		return medico;
 	}
+	
 	public void setMedico(Medico medico) {
 		this.medico = medico;
 	}
+	
+	
 	public Paciente getPaciente() {
 		return paciente;
 	}
+	
 	public void setPaciente(Paciente paciente) {
 		this.paciente = paciente;
 	}
+	
+	
 	public ArrayList<Exame> getExames() {
 		return exames;
 	}
+	
 	public void setExames(ArrayList<Exame> exames) {
 		this.exames = exames;
 	}
+	
 	
 	public void realizarCons() {
 		DemaisFunc.maisCons();
