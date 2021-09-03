d903e87b9dbd9882ed59e4eeabd836bf3cdf008e
Pedro Santana <80076882+PeSantanna@users.noreply.github.com>
2021-09-03 13:56:58 -0300

    paciente com Plano pronto

---
+++ src/PacCPlano.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class PacCPlano extends Paciente{
+	private int crm;
+	private int numConsRealiz;
+	private float somaConsMes;
+	private ArrayList<String> especialidades = new ArrayList<>();
+	private ArrayList<PlanoSaude> planosAtend = new ArrayList<>();
+	private ArrayList<Consulta> consulta = new ArrayList<>();
+	
+	
