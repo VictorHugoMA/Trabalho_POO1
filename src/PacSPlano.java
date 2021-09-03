5a00a693f0da75ded42a7c1ea24bd3f4be91cbfc
Guilherme R. Kameoka <80076658+guilhermekameoka@users.noreply.github.com>
2021-09-03 19:31:52 -0300

    Update PacSPlano.java

--- src/PacSPlano.java
+++ src/PacSPlano.java
@@ -1,4 +1,17 @@
 public class PacSPlano extends Paciente {
 	private boolean descEsp;
 	private float valorUltCons;
+	
+public boolean setDescEsp(int descEsp) {
+		if(DescEsp>0) {
+			this.descEsp = descEsp;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+public void setValorUltCons (float valorcons){
+	this.valorcons = valorcons;
 }
