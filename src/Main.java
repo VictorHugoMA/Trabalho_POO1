0ea9084e820bff8da35491e1b3993ffc411dc150
VictorHugoMA <victor.alves1@ufu.br>
2021-09-04 20:37:39 -0300

    add testes

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,22 @@ import java.text.ParseException;
 public class Main {
 
 	public static void main(String[] args) throws ParseException {
-		Pessoa p1 = new Pessoa();
 		Endereco end = new Endereco("Joao Naves", 1000);
+		PlanoSaude ps = new PlanoSaude("Unimed", 150f);
+		Exame ex1 = new Exame("Ressonancia");
+		Medico m1 = new Medico("Rafael", 200f);
+		DemaisFunc dm1 = new DemaisFunc("Jorge", "Recepcionista");
+		Paciente pac2 = new Paciente("Victor", "15635558674", "120", "Solteiro", end, "Masc", "20/11/2001");
+		PacCPlano pacC = new PacCPlano("Daniel", "88858413687", ps);
+		PacSPlano pacS = new PacSPlano("Davi", "73385255600");		
+		Consulta c1 = new Consulta(m1 ,pacC);
+		Consulta c2 = new Consulta(m1 ,pacS);
+		
 		
 		System.out.print("Teste CPF: ");
-		if(p1.setCpf("15635558674")) {
+		if(pac2.setCpf("15635558674")) {
 			System.out.println("Valido");
-			System.out.println(p1.getCpf());
+			System.out.println(pac2.getCpf());
 			
 		}
 		else {
@@ -18,29 +27,45 @@ public class Main {
 		System.out.println();
 		
 		
-		Medico m1 = new Medico();
 		
 		m1.setStrDataAdmi("11/09/2020");
-		System.out.println("Data Admissao do Medico: " + m1.getStrDataAdmi());
-		System.out.println();
+		System.out.println("Data Admissao do Medico " + m1.getNome()+": " + m1.getStrDataAdmi());
 		
+		DemaisFunc.setLimite(1);
+		DemaisFunc.setValorGrat(500f);
+		dm1.setSalarioBase(1100f);
 		
-		
-		Paciente pac1 = new Paciente();
-		Consulta c1 = new Consulta(m1 ,pac1);
 		
 		c1.setStrDataHoraCons("02/09/2021 15:30");
-		c1.setValor(200f);
-		System.out.println("Data e Hora da Consulta: " + c1.getStrDataHoraCons());
-		
+		c1.setFuncMarc(dm1);
 		c1.realizarCons();
-		System.out.println();
-		System.out.println("Data da Ultima consulta do Paciente: " + pac1.getStrDataUltCons());
-		System.out.println("Soma das Consultas do Medico: " + m1.getSomaConsMes());
+		c1.editarPront("Dor de cabeca", "Repouso e medicamento");
+		c1.addMedicamentos("Dorflex");
+		c1.addMedicamentos("Novalgina");
+		c1.addExames(ex1);
+		
+		c2.setStrDataHoraCons("04/09/2021 10:30");
+		c1.setFuncMarc(dm1);
+		c2.realizarCons();
+		c2.editarPront("Dor no dente", "Medicamento");
+		c2.addMedicamentos("Dipirona");
+		c2.addExames(ex1);
 		
 		
-		Paciente pac2 = new Paciente("Victor", "15635558674", "120", "Solteiro", end, "Masc", "20/11/2001");
 		System.out.println(pac2.mostraDados());
+		
+		
+		System.out.println(c1.mostrarDados());
+		System.out.println(c2.mostrarDados());
+		
+		System.out.println("\nData da Ultima consulta do Paciente " +pacC.getNome() + ": "+ pacC.getStrDataUltCons());
+		
+		System.out.println("\nNumero de consultas no mes: " + DemaisFunc.getNumConsTot());
+
+		System.out.println("\nSalario do Medico "+ m1.getNome()+ ": "+ m1.calcSalario());
+		
+		System.out.println("\nSalario do Funcionario "+ dm1.getNome()+ " (" + dm1.getCargo()+"): "+ dm1.calcSalario());
+		
 		
 		
 		
