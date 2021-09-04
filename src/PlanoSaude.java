a036c66548bff69d33c70411ae9c80f2692857f2
VictorHugoMA <victor.alves1@ufu.br>
2021-09-04 20:37:21 -0300

    add atributo do valor da consulta com plano

--- src/PlanoSaude.java
+++ src/PlanoSaude.java
@@ -3,12 +3,19 @@ public class PlanoSaude {
 	private String nome;
 	private String cnpj;
 	private Endereco endereco;
+	private float valorCons; //armazena o valor da consulta com plano
 	
 	
-	public PlanoSaude(String nome, String cnpj, Endereco endereco) {
+	public PlanoSaude(String nome, String cnpj, Endereco endereco, float valor) {
 		this.setNome(nome);
 		this.setCnpj(cnpj);
-		this.endereco = endereco;
+		this.setEndereco(endereco);
+		this.setValorCons(valor);
+	}
+	
+	public PlanoSaude(String nome, float valor) {
+		this.setNome(nome);
+		this.setValorCons(valor);
 	}
 	
 	public PlanoSaude() {
@@ -53,6 +60,23 @@ public class PlanoSaude {
 	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
+
+	
+	public float getValorCons() {
+		return valorCons;
+	}
+
+	public boolean setValorCons(float valorCons) {
+		if(valorCons>0) {
+			this.valorCons = valorCons;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	
 	
 	
 }
