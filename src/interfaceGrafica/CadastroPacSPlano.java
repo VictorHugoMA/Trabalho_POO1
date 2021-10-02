27fe3d3ccc17ccfda0fc7b00f0c38c0014fc6aa7
VictorHugoMA <victor.alves1@ufu.br>
2021-10-02 15:32:26 -0300

    att tela de cadastro

--- src/interfaceGrafica/CadastroPacSPlano.java
+++ src/interfaceGrafica/CadastroPacSPlano.java
@@ -1,29 +1,26 @@
 package interfaceGrafica;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.awt.FlowLayout;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
 import clinica.PacSPlano;
-
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.text.ParseException;
 
 public class CadastroPacSPlano extends JFrame {
 	private JLabel lblNewLabel;
-	private JTextField textField;
 	private JLabel lblNewLabel_1;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField textFieldCpf;
+	private JTextField textFieldRg;
+	private JTextField textFieldEstCivil;
+	private JTextField textFieldSexo;
+	private JTextField textFieldDatNasc;
+	private JTextField textFieldRua;
+	private JTextField textFieldNumEnd;
+	private JTextField textFieldCompl;
+	private JTextField textFieldBairro;
+	private JTextField textFieldCidade;
+	private JTextField textFieldCep;
+	private JTextField textFieldNome;
 
 	/**
 	 * Launch the application.
@@ -46,63 +43,177 @@ public class CadastroPacSPlano extends JFrame {
 	 */
 	public CadastroPacSPlano() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 700, 457);
 		getContentPane().setLayout(null);
 		
 		lblNewLabel = new JLabel("Nome:");
 		lblNewLabel.setBounds(10, 47, 51, 14);
 		getContentPane().add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setBounds(71, 44, 86, 20);
-		textField.setHorizontalAlignment(SwingConstants.CENTER);
-		getContentPane().add(textField);
-		textField.setColumns(10);
+		textFieldNome = new JTextField();
+		textFieldNome.setBounds(83, 44, 86, 20);
+		getContentPane().add(textFieldNome);
+		textFieldNome.setColumns(10);
 		
 		lblNewLabel_1 = new JLabel("CPF:");
 		lblNewLabel_1.setBounds(10, 75, 51, 14);
 		getContentPane().add(lblNewLabel_1);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(71, 72, 86, 20);
-		getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+		textFieldCpf = new JTextField();
+		textFieldCpf.setBounds(83, 72, 86, 20);
+		getContentPane().add(textFieldCpf);
+		textFieldCpf.setColumns(10);
 		
-		JLabel lblNewLabel_2 = new JLabel("Cadastro Paciente");
-		lblNewLabel_2.setBounds(169, 11, 102, 14);
+		JLabel lblNewLabel_2 = new JLabel("Cadastro Paciente sem Plano");
+		lblNewLabel_2.setBounds(233, 11, 168, 14);
 		getContentPane().add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("RG:");
 		lblNewLabel_3.setBounds(10, 103, 51, 14);
 		getContentPane().add(lblNewLabel_3);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(71, 100, 86, 20);
-		getContentPane().add(textField_2);
-		textField_2.setColumns(10);
+		textFieldRg = new JTextField();
+		textFieldRg.setBounds(83, 100, 86, 20);
+		getContentPane().add(textFieldRg);
+		textFieldRg.setColumns(10);
 		
 		JButton btnNewButton = new JButton("Enviar");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String nome = textField.getText();
+				String nome = textFieldNome.getText();
 				PacSPlano p = new PacSPlano();
 				p.setNome(nome);
+				p.setCpf(textFieldCpf.getText());
+				p.setRg(textFieldRg.getText());
+				p.setEstadoCivil(textFieldEstCivil.getText());
+				p.setSexo(textFieldSexo.getText());
+				try {
+					p.setStrDataNasc(textFieldDatNasc.getText());
+				} catch (ParseException e1) {
+					
+				}
+				//Endereco
+				//Desconto especial
+				
 			}
 		});
-		btnNewButton.setBounds(236, 227, 89, 23);
+		btnNewButton.setBounds(312, 384, 89, 23);
 		getContentPane().add(btnNewButton);
 		
 		JLabel lblNewLabel_4 = new JLabel("Est. Civil:");
 		lblNewLabel_4.setBounds(10, 128, 51, 14);
 		getContentPane().add(lblNewLabel_4);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(71, 125, 86, 20);
-		getContentPane().add(textField_3);
-		textField_3.setColumns(10);
+		textFieldEstCivil = new JTextField();
+		textFieldEstCivil.setBounds(83, 125, 86, 20);
+		getContentPane().add(textFieldEstCivil);
+		textFieldEstCivil.setColumns(10);
 		
 		JButton btnNewButton_1 = new JButton("Voltar");
-		btnNewButton_1.setBounds(137, 227, 89, 23);
+		btnNewButton_1.setBounds(213, 384, 89, 23);
 		getContentPane().add(btnNewButton_1);
+		
+		JLabel lblNewLabel_5 = new JLabel("Sexo:");
+		lblNewLabel_5.setBounds(10, 153, 46, 14);
+		getContentPane().add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("Data Nasc:");
+		lblNewLabel_6.setBounds(10, 178, 69, 14);
+		getContentPane().add(lblNewLabel_6);
+		
+		textFieldSexo = new JTextField();
+		textFieldSexo.setBounds(83, 150, 86, 20);
+		getContentPane().add(textFieldSexo);
+		textFieldSexo.setColumns(10);
+		
+		textFieldDatNasc = new JTextField();
+		textFieldDatNasc.setBounds(83, 175, 86, 20);
+		getContentPane().add(textFieldDatNasc);
+		textFieldDatNasc.setColumns(10);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(10, 206, 664, 2);
+		getContentPane().add(separator);
+		
+		JLabel lblNewLabel_7 = new JLabel("Endere\u00E7o");
+		lblNewLabel_7.setBounds(10, 214, 60, 23);
+		getContentPane().add(lblNewLabel_7);
+		
+		JLabel lblNewLabel_8 = new JLabel("Rua");
+		lblNewLabel_8.setBounds(10, 242, 30, 14);
+		getContentPane().add(lblNewLabel_8);
+		
+		textFieldRua = new JTextField();
+		textFieldRua.setBounds(49, 239, 86, 20);
+		getContentPane().add(textFieldRua);
+		textFieldRua.setColumns(10);
+		
+		JLabel lblNewLabel_9 = new JLabel("Numero");
+		lblNewLabel_9.setBounds(145, 242, 46, 14);
+		getContentPane().add(lblNewLabel_9);
+		
+		textFieldNumEnd = new JTextField();
+		textFieldNumEnd.setBounds(201, 239, 86, 20);
+		getContentPane().add(textFieldNumEnd);
+		textFieldNumEnd.setColumns(10);
+		
+		JLabel lblNewLabel_10 = new JLabel("Complemento");
+		lblNewLabel_10.setBounds(297, 242, 86, 14);
+		getContentPane().add(lblNewLabel_10);
+		
+		textFieldCompl = new JTextField();
+		textFieldCompl.setBounds(384, 239, 86, 20);
+		getContentPane().add(textFieldCompl);
+		textFieldCompl.setColumns(10);
+		
+		JLabel lblNewLabel_11 = new JLabel("Bairro");
+		lblNewLabel_11.setBounds(10, 267, 46, 14);
+		getContentPane().add(lblNewLabel_11);
+		
+		textFieldBairro = new JTextField();
+		textFieldBairro.setBounds(49, 264, 86, 20);
+		getContentPane().add(textFieldBairro);
+		textFieldBairro.setColumns(10);
+		
+		JLabel lblNewLabel_12 = new JLabel("Cidade");
+		lblNewLabel_12.setBounds(145, 267, 46, 14);
+		getContentPane().add(lblNewLabel_12);
+		
+		textFieldCidade = new JTextField();
+		textFieldCidade.setBounds(201, 264, 86, 20);
+		getContentPane().add(textFieldCidade);
+		textFieldCidade.setColumns(10);
+		
+		JLabel lblNewLabel_13 = new JLabel("CEP");
+		lblNewLabel_13.setBounds(322, 267, 25, 14);
+		getContentPane().add(lblNewLabel_13);
+		
+		textFieldCep = new JTextField();
+		textFieldCep.setBounds(384, 264, 86, 20);
+		getContentPane().add(textFieldCep);
+		textFieldCep.setColumns(10);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(10, 295, 664, 2);
+		getContentPane().add(separator_1);
+		
+		JLabel lblNewLabel_14 = new JLabel("Possui desconto especial ?");
+		lblNewLabel_14.setBounds(10, 308, 159, 14);
+		getContentPane().add(lblNewLabel_14);
+		
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Sim");
+		rdbtnNewRadioButton.setBounds(10, 323, 109, 23);
+		getContentPane().add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("N\u00E3o");
+		rdbtnNewRadioButton_1.setBounds(10, 345, 109, 23);
+		getContentPane().add(rdbtnNewRadioButton_1);
+		
+		ButtonGroup bg = new ButtonGroup();
+		bg.add(rdbtnNewRadioButton);
+		bg.add(rdbtnNewRadioButton_1);
+		
+		
 	}
 }
